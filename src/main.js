d0437d2676411e360b22cc9659f91bc43247b0b6
mertarslan16 <mert.arslan1963@gmail.com>
2023-11-24 17:38:49 +0300

    first

--- src/main.js
+++ src/main.js
@@ -5,13 +5,13 @@ import router from "./router";
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
-    apiKey: "Axxxxxxxxxxxxxxxxxxxx14",
-    authDomain: "vue-auth-xxxxxx.firebaseapp.com",
-    databaseURL: "https://vue-auth-xxxxxx.firebaseio.com",
-    projectId: "vue-auth-xxxxxx",
-    storageBucket: "vue-auth-xxxxxx.appspot.com",
-    messagingSenderId: "2xxxxxxxxxxxxxxx1",
-    appId: "1:2xxxxxxxxxxxxx1:web:xxxxxxxxxxxxxxxxx"
+  apiKey: "AIzaSyAH8tiMMzMOc0C_Be4I3OUMvcP6sjjT0IA",
+  authDomain: "vueapp-f3b90.firebaseapp.com",
+  projectId: "vueapp-f3b90",
+  storageBucket: "vueapp-f3b90.appspot.com",
+  messagingSenderId: "638193987764",
+  appId: "1:638193987764:web:2fcdad09694d588da5a7e1",
+  measurementId: "G-0DD9JW9TXY"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
